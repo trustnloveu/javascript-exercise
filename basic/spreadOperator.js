60b2bc9461fbc45b26cb6057d28df729d49a143b
austin <trustnloveu@gmail.com>
2020-11-21 18:04:16 +0900

    Spread Operator
    Sum up given numbers which might be inside array or not

--- basic/spreadOperator.js
+++ basic/spreadOperator.js
@@ -1,15 +1,21 @@
-function sum(args) {
-  const flag = Array.isArray(args);
-  if (!flag) {
-    console.log("passed arguments are not an array of numbers.");
-    return;
-  } else {
-    const numbers = [...args];
-    console.log(numbers.reduce((total, number) => total + number));
-    return;
-  }
-}
+// 인자가 숫자 배열일 때, 배열 내 숫자의 합
+// 인자가 복수의 숫자일 때, 모든 인자의 합
+// 함수를 구현하시오.
 
+// My solution
+// function sum(args) {
+//   const flag = Array.isArray(args);
+//   if (!flag) {
+//     console.log("passed arguments are not an array of numbers.");
+//     return;
+//   } else {
+//     const numbers = [...args];
+//     console.log(numbers.reduce((total, number) => total + number));
+//     return;
+//   }
+// }
+
+// Correct Solution
 function sum(...items) {
   if (items.length === 1 && Array.isArray(items[0])) items = [...items[0]];
   console.log(items.reduce((a, b) => a + b));
